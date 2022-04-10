fe7093fe007d5a537fa8da2557b03ac391013ba6
Jeff Kaufman <jeff@jefftk.com>
2022-04-10 10:33:21 -0400

    more analysis

--- process.py
+++ process.py
@@ -1,4 +1,18 @@
+import sys
 from collections import namedtuple
+
+NOTES=["I",
+       "bI",
+       "II",
+       "bIII",
+       "III",
+       "IV",
+       "bV",
+       "V",
+       "bVI",
+       "VI",
+       "bVII",
+       "VII"]
 
 ParsedTune = namedtuple(
   'ParsedTune', ['raw', 'key', 'chords'])
@@ -37,19 +51,6 @@ def parse_tune(line):
                     key=raw_chords[0],
                     chords=chords)
 
-NOTES=["I",
-       "bI",
-       "II",
-       "bIII",
-       "III",
-       "IV",
-       "bV",
-       "V",
-       "bVI",
-       "VI",
-       "bVII",
-       "VII"]
-
 def interpret_chord(root, chord):
   delta_num = (chord.num - root.num + 12) % 12
   delta_text = NOTES[delta_num]
@@ -57,46 +58,108 @@ def interpret_chord(root, chord):
     delta_text = delta_text.lower()
   return delta_text
 
-data = []
-
-def process(fname):
-  with open(fname) as inf:
-    for line in inf:
-      data.append(parse_tune(line.strip()))
-
-process("f-and-w.txt")
-process("t.txt")
-      
-counts = []
-for note in NOTES:
-  counts.append([0, 0])
+def load(fnames):
+  tunes = []
+  for fname in fnames:
+    with open(fname) as inf:
+      for line in inf:
+        line = line.strip()
+        if line:
+          tunes.append(parse_tune(line))
+  return tunes
 
 def reinterpret(tune, root):
   return [interpret_chord(root, chord)
           for chord in tune.chords]
-  
-for tune in data:
-  root = tune.chords[0]
 
-  if True:  # reinterpret
-    if root.is_minor: # minor, think of as vi
+def evaluate(tunes,
+             options="",
+             graph=False,
+             relative_minor=True,
+             mixolydian=True,
+             thirds=True):
+  if type(options) == type(""):
+    options = options.split()
+
+  if not thirds:
+    options = [option.lower() for option in options]
+
+  counts = []  # note index -> [major count, minor count]
+  for note in NOTES:
+    counts.append([0, 0])
+
+  playable = 0
+
+  for tune in tunes:
+    root = tune.chords[0]
+
+    if relative_minor and root.is_minor: # minor, think of as vi
       root = ParsedChord(raw="_", num=tune.chords[0].num + 3, is_minor=False)
-    elif "bVII" in reinterpret(tune, root): # mixolydian, think of as V
+    elif mixolydian and \
+         "bVII" in reinterpret(tune, root): # mixolydian, think of as V
       root = ParsedChord(raw="_", num=tune.chords[0].num + 5, is_minor=False)
 
-  interpreted = reinterpret(tune, root)
+    interpreted = reinterpret(tune, root)
+    if options:
+      if not thirds:
+        interpreted = [chord.lower() for chord in interpreted]
+      if all(chord in options for chord in interpreted):
+        #print ("%20s   %20s" % (tune.raw, " ".join(interpreted)))
+        playable += 1
+    else:
+      for i, note in enumerate(NOTES):
+        if note in interpreted:
+          counts[i][0] += 1
+        if note.lower() in interpreted:
+          counts[i][1] += 1
 
-  for i, note in enumerate(NOTES):
-    if note in interpreted:
-      counts[i][0] += 1
-    if note.lower() in interpreted:
-      counts[i][1] += 1
+  if options:
+    #print("%.2f" % (playable / len(tunes)))
+    return playable / len(tunes)
+  else:
+    if thirds:
+      for i, note in enumerate(NOTES):
+        for j, full_note in enumerate([note, note.lower()]):
+          if graph:
+            print("%4d  %4s: %s" % (counts[i][j], full_note, "*" * counts[i][j]))
+          else:
+            print("%s\t%s" % (full_note, counts[i][j]))
+    else:
+      for i, note in enumerate(NOTES):
+        count = counts[i][0] + counts[i][1]
+        if graph:
+          print("%4d  %4s: %s" % (count, note, "*" * count))
+        else:
+          print("%s\t%s" % (note, count))
 
-if True:  # consider include thirds, distinguishing minor/major
-  for i, note in enumerate(NOTES):
-    for j, full_note in enumerate([note, note.lower()]):
-      print("%4d  %4s: %s" % (counts[i][j], full_note, "*" * counts[i][j]))
-else:
-  for i, note in enumerate(NOTES):
-    count = counts[i][0] + counts[i][1]
-    print("%4d  %4s: %s" % (count, note, "*" * count))
+def consider(tunes, options_list, table=True):
+  if table:
+    print("<table border=1, cellpadding=5>")
+    print("<tr><th>Chords<td>Thirds<td>Open")
+
+  for options in options_list:
+    if table:
+      print ("<tr><td>%s<td>%.0f%%<td>%.0f%%" % (
+        options,
+        evaluate(tunes, options, thirds=True) * 100,
+        evaluate(tunes, options, thirds=False) * 100))
+    else:
+      print("%.2f\t%.2f\t%s" % (
+        evaluate(tunes, options, thirds=True),
+        evaluate(tunes, options, thirds=False),
+        options))
+
+  if table:
+    print ("</table>")
+
+if __name__ == "__main__":
+  tunes = load(sys.argv[1:])
+  #evaluate(tunes)
+  consider(tunes, [
+    "I IV V",
+    "I IV V vi",
+    "I IV V vi",
+    "I iii IV V vi",
+    "I ii IV V vi",
+    "I ii iii IV V vi",
+  ])
